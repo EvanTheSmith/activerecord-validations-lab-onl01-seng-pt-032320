b06527b3b6bfef471d3958cb75885bbbb7a88d46
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-08-04 12:14:48 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validate :title_has_spam
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    def title_has_spam
+        clickbait = Array.[]("Won't Believe", "Secret", "Top [number]", "Guess")
+        if title && clickbait.none? {|click| title.include?(click)}
+          errors.add(:spam, "Title must be clickbaity.")
+        end
+    end
 end
